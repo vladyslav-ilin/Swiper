c56ae3ed5d7e832c397acb7ae54a1fc6b3f7411e
vladyslav-ilin <vlad110696@gmail.com>
2021-01-01 22:25:45 +0200

    Swiper

---
+++ js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+new Swiper('.slider__items', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 3.6,
+  spaceBetween: 45,
+  loop: true, 
+  freeMode: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteractive: false
+  },
+  speed: 800,
+});
